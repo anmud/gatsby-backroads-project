f07786d254d4e002faf07cda97176e10c6a4ad8b
anmud <anmud@list.ru>
2019-07-10 12:39:07 +0200

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  siteMetadata: {
+    title: "Backroads",
+    description: "Explore awesome worldwide tours and discover what makes each of them unique. Forget your daily routine & say yes to adventure",
+    author: '@anmud',
+  
+  },
   plugins: [
     
     `gatsby-plugin-styled-components`,
