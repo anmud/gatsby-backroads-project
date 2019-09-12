e14e7268acfe9f9257d65e36823ea670fed07f6f
anmud <anmud@list.ru>
2019-09-12 16:03:09 +0200

    twitter card updated

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import {Helmet} from 'react-helmet'
-import {useStaticQuery, graphql} from 'gatsby'
+import React from "react"
+import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
 
 const getData = graphql`
-query{
-    site{
-      siteMetadata{
+  query {
+    site {
+      siteMetadata {
         siteTitle: title
         siteDesc: description
         author
@@ -17,24 +17,29 @@ query{
   }
 `
 
-const SEO = ({title, description}) => {
+const SEO = ({ title, description }) => {
+  const { site } = useStaticQuery(getData)
 
-const {site} = useStaticQuery(getData)
+  const {
+    siteDesc,
+    siteTitle,
+    siteUrl,
+    image,
+    twitterUsername,
+  } = site.siteMetadata
 
-const {siteDesc, siteTitile, siteUrl, image, twitterUsername} = site.siteMetadata
-
-    return (
-      <Helmet htmlAttributes={{lang: 'en'}} title={`${title} | ${siteTitile}`}>
-        <meta name='description' content={description || siteDesc}/>
-        <meta name='image' content={image}/>
-
-        <meta name='twitter:card' content='summary_large_image'/>
-        <meta name='twitter:creator' content={twitterUsername}/>
-        <meta name='twitter:title' content={siteTitile}/>
-        <meta name='twitter:description' content={description}/>
-        <meta name='twitter:image' content={`${siteUrl}${image}`}/>
-      </Helmet>
-    )
+  return (
+    <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
+      <meta name="description" content={description || siteDesc} />
+      <meta name="image" content={image} />
+      {/* twitter card */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={twitterUsername} />
+      <meta name="twitter:title" content={siteTitle} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={`${siteUrl}${image}`} />
+    </Helmet>
+  )
 }
 
-export default SEO;
+export default SEO
